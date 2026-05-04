e4a887276b5dde9f66a5aac99564cdbc0fe19fe9
M-Sravanthi <86660902+M-Sravanthi@users.noreply.github.com>
2026-05-04 14:17:15 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+
+month = int(sys.argv[1])
+df = pd.DataFrame({"day":[1,2], "num_pax":[3,4]})
+df['month'] = month
+print(df.head())
+
+df.to_parquet(f"output_{month}.parquet")
+print(f"Loading pipelines for month: {month}" )
+
