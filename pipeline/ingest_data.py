3ae273f89465928d6bd84b6abf01e87e09fae1d3
M-Sravanthi <86660902+M-Sravanthi@users.noreply.github.com>
2026-05-15 15:12:25 +0000

    Modified pipeline

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -5,6 +5,7 @@
 #get_ipython().system('uv add sqlalchemy')
 #get_ipython().system('uv add psycopg2-binary')
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -43,44 +44,45 @@ parse_dates = [
 # This dataset is too big. So now we will only read chunks of this dataset at once and then insert the chunks into the table.
 
 
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-
-
-    year = 2021
-    month =1
-
-    target_table = 'yello_taxi_data'
-    chunksize = 100000
-
+@click.command()
+@click.option('--pg-user', default='root', show_default=True, help='PostgreSQL username')
+@click.option('--pg-pass', default='root', show_default=True, help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', show_default=True, help='PostgreSQL host')
+@click.option('--pg-port', default=5432, show_default=True, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', show_default=True, help='PostgreSQL database name')
+@click.option('--year', default=2021, show_default=True, type=int, help='Year of the taxi dataset')
+@click.option('--month', default=1, show_default=True, type=int, help='Month of the taxi dataset')
+@click.option('--target-table', default='yello_taxi_data', show_default=True, help='Target PostgreSQL table name')
+@click.option('--chunksize', default=100000, show_default=True, type=int, help='Number of rows to read per chunk')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    #posgresql://user:pass@localhost:port_No/db_name
+    # postgresql://user:pass@localhost:port/db_name
     engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
 
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
+    )
 
-    df_iter = pd.read_csv(url,
-                     dtype=dtype,
-                     parse_dates=parse_dates,
-                     iterator = True,
-                     chunksize = chunksize)
     first = True
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(0).to_sql(
                 name=target_table,
                 con=engine,
-                if_exists='replace')
+                if_exists='replace',
+            )
             first = False
         df_chunk.to_sql(
             name=target_table,
             con=engine,
-            if_exists='append')
+            if_exists='append',
+        )
 
 if __name__ == '__main__':
     run()
