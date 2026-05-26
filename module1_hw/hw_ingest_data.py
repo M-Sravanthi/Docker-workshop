f4cafd2c0e86180fb9feee6e4705d1a60bafbc52
M-Sravanthi <86660902+M-Sravanthi@users.noreply.github.com>
2026-05-26 11:13:36 +0000

    Completed ingest data HW

--- module1_hw/hw_ingest_data.py
+++ module1_hw/hw_ingest_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
@@ -29,55 +30,53 @@ parse_dates = [
     "lpep_dropoff_datetime"
 ]
 
-def run(pg_user,pg_pass,pg_port,pg_host,pg_db,target_table,month,year,chunksize):
+@click.command()
+@click.option('--pg_user', default='root', help='Postgres user')
+@click.option('--pg_pass', default='root', help='Postgres password')
+@click.option('--pg_host', default='pgdatabase', help='Postgres host')
+@click.option('--pg_port', default=5432, type=int, help='Postgres port')
+@click.option('--pg_db', default='ny_taxi', help='Postgres database name')
+@click.option('--year', default=2025, type=int, help='Year of data')
+@click.option('--month', default=11, type=int, help='Month of data')
+@click.option('--target_table', default='green_taxi_data1', help='Target table name')
+@click.option('--chunksize', default=5000, type=int, help='CSV read chunksize')
 
-    pg_user = 'root'
-    pg_pass ='root'
-    pg_host='localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-    month = 11
-    year = 2025
-    target_table= 'green_taxi_data'
-    chunksize = 5000
+def run(pg_user, pg_pass, pg_port, pg_host, pg_db, target_table, month, year, chunksize):
+    """Ingest green taxi data for a given year/month into Postgres."""
 
-    prefix = "https://d37ci6vzurychx.cloudfront.net/trip-data/"
-    url = f'{prefix}/green_tripdata_{year}-{month:02d}.parquet'
+    prefix = "https://d37ci6vzurychx.cloudfront.net/trip-data"
+    url = f"{prefix}/green_tripdata_{year}-{month:02d}.parquet"
     df_parquet = pd.read_parquet(url)
 
-    df_csv = df_parquet.to_csv("green_tripdata_2025-11.csv", index=False)
+    csv_file = f"green_tripdata_{year}-{month:02d}.csv"
+    df_parquet.to_csv(csv_file, index=False)
 
-    #posgresql://user:pass@localhost:port_No/db_name
+    # postgresql://user:pass@localhost:port/db_name
     engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
 
     df_iter = pd.read_csv(
-        df_csv,
+        csv_file,
         dtype=dtype,
         parse_dates=parse_dates,
-        Iterator = True,
-        chunksize=chunksize
-        )
+        iterator=True,
+        chunksize=chunksize,
+    )
 
     first = True
     for df_chunk in tqdm(df_iter):
         if first:
-            df_chunk.head(0).to_sql(
-                        name=target_table,
-                        con=engine,
-                        if_exists='replace')
+            df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists="replace")
             first = False
-            df_chunk.to_sql(
-                        name=target_table,
-                        con=engine,
-                        if_exists='append',
-            )
+
+        df_chunk.to_sql(name=target_table, con=engine, if_exists="append")
     #Ingesting zones lookup csv file
     zone_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv'
 
     zones_df = pd.read_csv(zone_url)
     zones_df.to_sql(con=engine, 
-                    name='zones_lookup', 
+                    name='zones_lookup1', 
                     if_exists='replace')
+
 
 
 if __name__ == '__main__':
